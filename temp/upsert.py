c23565c591fe6a0b2525145e815818a7dc65ff07
phucdm04 <dmp041204@gmail.com>
2025-06-15 16:19:44 +0700

    Add temp files

--- temp/upsert.py
+++ temp/upsert.py
@@ -14,13 +14,19 @@ from tqdm import tqdm
 from database_connector.mongodb_connector import load_documents
 from preprocessing.preprocessing import LSASVDPipeline, WordEmbeddingPipeline
 
-def process_and_upsert(docs, model, qdrant_client, qdrant_collection_name):
+def process_and_upsert(docs, model, qdrant_client, qdrant_collection_name, overwrited = True):
     processed_docs = [doc["cleaned_description"] for doc in docs]
     vectors = model.transform_doc(processed_docs)
     vector_size = len(vectors[0])
+
     
     if not ensure_collection_exists(qdrant_client, qdrant_collection_name, vector_size=vector_size):
         create_collection(qdrant_client, qdrant_collection_name, vector_size=vector_size)
+    else:
+        if overwrited:
+            print(f"Recreate collection {qdrant_collection_name}")
+            qdrant_client.delete_collection(collection_name=qdrant_collection_name)
+            create_collection(qdrant_client, qdrant_collection_name, vector_size=vector_size)
 
     for i, _ in enumerate(tqdm(processed_docs, desc="Upserting")):
         
@@ -44,14 +50,14 @@ if __name__ == "__main__":
     uri = os.getenv("MONGO_URI")
     db_name = os.getenv("DATABASE_NAME")
     
-    collection_name = os.getenv("LSA_COLLECTION_NAME")
+    collection_name = os.getenv("WEMB_COLLECTION_NAME")
     docs = load_documents(uri, db_name, collection_name)
     processed_docs = [doc["cleaned_description"] for doc in docs]
 
-    with open("./trained_models/tfidf.pkl", "rb") as f:
+    with open("./trained_models/hellinger_pca.pkl", "rb") as f:
         embedder = pickle.load(f)
 
-    process_and_upsert(docs[:10], embedder, client, "tfidf_test")
+    process_and_upsert(docs[:], embedder, client, "hellinger_pca")
     # vectors = embedder.transform_doc(processed_docs)
 
 
