b4e169b61b8f4e88cae0fbdef84ed30d0a31ce84
phucdm04 <dmp041204@gmail.com>
2025-06-28 16:22:23 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,26 +1,20 @@
+
 from flask import Flask, request, jsonify, render_template
-from pyngrok import ngrok
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 from search_engine.search_logic import search_query
-from embedding.ppmi import PPMIEmbedder, TruncatedSVD 
 from embedding.FastText import FastTextLSAEmbedder, FastText, TruncatedSVD
-
-
 
 load_dotenv()
 
 app = Flask(__name__)
-ngrok.set_auth_token(os.getenv("NGROK_TOKEN"))
-
 
 # Kết nối MongoDB
 uri = os.getenv("MONGO_URI")
 client = MongoClient(uri, tls=True, tlsAllowInvalidCertificates=True)
 db = client["Film"]
 collection = db["Data"]
-
 
 @app.route("/")
 def home():
@@ -37,9 +31,6 @@ def search():
     model = data.get("model")
     hits = search_query(query, model)
 
-    # query = data.get("query")
-    # hits = search_query(query)
-    # Chuyển đổi từng ScoredPoint thành dict
     results = [
         {
             "id": hit.id,
@@ -52,8 +43,4 @@ def search():
     return jsonify(results)
 
 if __name__ == "__main__":
-    # Mở cổng 5000 bằng ngrok
-    public_url = ngrok.connect(5000)
-    print(" * Ngrok public URL:", public_url)
-    
     app.run(port=5000)
